29cba6becf83ad51578cff7653c03dda69b41159
lkepitter <lkepitter@hotmail.co.uk>
2022-05-24 17:42:24 +0100

    Added utils folder for utility functions etc. Added dummy data for categories, genres, keywords. Modals now display relevant data for current book. Added styling for modals.

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import css from "./modal.module.css";
+import { convertPluralToSingular } from "../../utilities/functions.js";
 
 function Modal({ editField, loadedData }) {
   function closeModal() {
@@ -12,36 +13,51 @@ function Modal({ editField, loadedData }) {
     };
     window.onclick = function (event) {
       console.log("Window clicked");
-      //const modal = document.getElementById(editField + "Modal");
       if (event.target === modal) {
         modal.style.display = "none";
       }
     };
   }
 
+  //Convert the editField into a title
+  const fieldTitle = [editField[0].toUpperCase(), ...editField.slice(1)];
+  //Convert the editField into a singular
+  const fieldSingular = convertPluralToSingular(editField);
+
   return (
     <div id={`${editField}Modal`} className={css.modal} display="block">
       <div className={css.modalContent}>
-        <span className={css.modalClose} onClick={() => closeModal()}>
-          &times;
-        </span>
-        Hello! I am {editField}
+        <div className={css.modalHead}>
+          <span className={css.modalClose} onClick={() => closeModal()}>
+            &times;
+          </span>
+          {fieldTitle}
+        </div>
         <div className={css.current}>
-          List the current data for the field here.
+          Below are all {editField} associated with this book. Click on one to
+          remove it.
           {loadedData[editField]
             ? loadedData[editField].map((item) => {
-                return <div key={item}>{item}</div>;
+                return (
+                  <div className={css.fieldItem}>
+                    <div key={item}>{item}</div>
+                  </div>
+                );
               })
             : ""}
         </div>
         <div className={css.add}>
-          List the data that can be added to the field here. (i.e. all possible
-          options in the database)
+          Click on a {fieldSingular} from the list below to add it to the
+          current book.
           {/*allData[editField].map*/}
         </div>
-        <button>Confirm</button>
-        <button>Cancel</button>
-        <button>Add New</button>
+        <div className={css.currentFoot}>
+          <button>Confirm</button>
+          <button>Cancel</button>
+        </div>
+        <div className={css.addFoot}>
+          <button>Create New</button>
+        </div>
       </div>
     </div>
   );
